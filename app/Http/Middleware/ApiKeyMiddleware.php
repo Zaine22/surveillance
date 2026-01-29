e238eb9de35611913db8f0d888b2f059530a9a64
Htet Aung <htetaung.dev@gmail.com>
2026-01-29 16:52:48 +0630

    added changes

---
+++ app/Http/Middleware/ApiKeyMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiKeyMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKey = $request->header('x-api-key');
+
+        // store your key in .env
+        if (!$apiKey || $apiKey !== config('app.api_key')) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
