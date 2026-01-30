ec8fe8683837228ff0f5e44f4c5a4165adad12ec
Zaine <kyawkokozin223@gmail.com>
2026-01-30 13:09:05 +0630

    fix :  syntax

--- app/Services/CaseManagementService.php
+++ app/Services/CaseManagementService.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\Services;
 
-use Illuminate\Support\Str;
+use App\Models\AiPredictResult;
 use App\Models\CaseFeedback;
 use App\Models\CaseManagement;
-use App\Models\AiPredictResult;
 use App\Models\CaseManagementItem;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CaseManagementService
 {
@@ -37,37 +37,36 @@ class CaseManagementService
 
             return $case;
         });
+    }
+    public function createCaseFeedback(array $data): CaseFeedback
+    {
+        $case = CaseFeedback::updateOrCreate(
+            ['case_id' => $data['case_id']],
+            [
+                'url'         => $data['url'],
+                'is_illegal'  => $data['is_illegal'],
+                'legal_basis' => $data['legal_basis'] ?? null,
+                'reason'      => $data['reason'] ?? null,
+            ]
+        );
 
-        function createCaseFeedback(array $data): CaseFeedback
-        {
-            $case = CaseFeedback::updateOrCreate(
-                ['case_id' => $data['case_id']],
-                [
-                    'url'         => $data['url'],
-                    'is_illegal'  => $data['is_illegal'],
-                    'legal_basis' => $data['legal_basis'] ?? null,
-                    'reason'      => $data['reason'] ?? null,
-                ]
-            );
+        return $case;
+    }
 
-            return $case;
-        }
+    public function createExternalCase(array $data): CaseManagementItem
+    {
+        $external_case_id = 'EXT-' . strtoupper(uniqid());
+        $case             = CaseManagement::create([
+            'external_case_no' => $external_case_id,
+        ]);
 
-        function createExternalCase(array $data): CaseManagementItem
-        {
-            $external_case_id = 'EXT-' . strtoupper(uniqid());
-            $case             = CaseManagement::create([
-                'external_case_no' => $external_case_id,
-            ]);
+        $caseItem = CaseManagementItem::create([
+            'case_management_id' => $case->id,
+            'crawler_page_url'   => $data['url'],
+            'status'             => 'valid',
+            'reason'             => $data['leakReason'],
+        ]);
 
-            $caseItem = CaseManagementItem::create([
-                'case_management_id' => $case->id,
-                'crawler_page_url'   => $data['url'],
-                'status'             => 'valid',
-                'reason'             => $data['leakReason'],
-            ]);
-
-            return $caseItem;
-        }
+        return $caseItem;
     }
 }
