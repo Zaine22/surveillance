bc55b3b67b116b150beee8ba45938efd82106e4b
Zaine <kyawkokozin223@gmail.com>
2026-02-27 13:51:14 +0630

    fix : add prejudgement id  to audit resource

--- app/Http/Resources/AiPredictResultAuditResource.php
+++ app/Http/Resources/AiPredictResultAuditResource.php
@@ -14,13 +14,14 @@ class AiPredictResultAuditResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'            => $this->id,
-            'decision'      => $this->decision,
-            'file_name'     => $this->result?->aiModelTask?->file_name,
-            'valid_count'   => $this->valid_count,
-            'invalid_count' => $this->invalid_count,
-            'auditor'       => $this->auditor->name ?? null,
-            'created_at'    => $this->created_at,
+            'id'                => $this->id,
+            'decision'          => $this->decision,
+            'file_name'         => $this->result?->aiModelTask?->file_name,
+            'ai_predict_result_id' => $this->ai_predict_result_id,
+            'valid_count'       => $this->valid_count,
+            'invalid_count'     => $this->invalid_count,
+            'auditor'           => $this->auditor->name ?? null,
+            'created_at'        => $this->created_at,
         ];
     }
 }
