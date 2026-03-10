ff2894bac201e86a4214289cf2e288aee089beff
Zaine <kyawkokozin223@gmail.com>
2026-03-10 09:31:11 +0630

    fix : sync crawlerfilejob

--- app/Jobs/SyncCrawlerFileJob.php
+++ app/Jobs/SyncCrawlerFileJob.php
@@ -13,17 +13,16 @@ class SyncCrawlerFileJob implements ShouldQueue
 
     public function __construct(
         public CrawlerTaskItem $item,
-        protected AiTaskManagerService $aiTaskManagerService
     ) {}
 
-    public function handle(DataSyncOrchestratorService $orchestrator): void
+    public function handle(DataSyncOrchestratorService $orchestrator,AiTaskManagerService $aiTaskManagerService): void
     {
         \Illuminate\Support\Facades\Log::info('SyncCrawlerFileJob now handling file sync', [
             'item_id'     => $this->item->id,
             'result_file' => $this->item->result_file,
         ]);
         $orchestrator->syncCrawlerFileToNas($this->item);
-        $this->aiTaskManagerService->createFromCrawlerItem($this->item);
+        $aiTaskManagerService->createFromCrawlerItem($this->item);
 
     }
 }
