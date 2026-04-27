5ed18c6839a1f2bc9d09bf14aa64a43d7eb17eee
Zaine <kyawkokozin223@gmail.com>
2026-04-27 09:41:30 +0630

    fix : email format

--- app/Http/Requests/Auth/SendOtpRequest.php
+++ app/Http/Requests/Auth/SendOtpRequest.php
@@ -22,7 +22,24 @@ class SendOtpRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
+            'email' => [
+                'required',
+                'email:rfc,dns',
+                function ($attribute, $value, $fail) {
+                    $domain = strtolower(substr(strrchr($value, "@"), 1));
+
+                    $blockedDomains = [
+                        'gmail.co',
+                        'gamil.com',
+                        'gmial.com',
+                        'gnail.com',
+                    ];
+
+                    if (in_array($domain, $blockedDomains, true)) {
+                        $fail('邮箱格式不正确');
+                    }
+                },
+            ],
         ];
     }
 
